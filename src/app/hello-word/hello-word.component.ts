778de732ea9a156d67befbe13c7b58d241f1e458
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-02 16:53:11 -0700

    bind data in component, render in template.

--- src/app/hello-word/hello-word.component.ts
+++ src/app/hello-word/hello-word.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloWordComponent implements OnInit {
 
+  message = 'Hello from Hello World Component!';
+
   constructor() { }
 
   ngOnInit() {
