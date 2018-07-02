ba06af26f5ec317eb647b8e868c3f6f12665eda1
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-02 16:46:24 -0700

    ng generate component hello-word

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWordComponent } from './hello-word/hello-word.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWordComponent
   ],
   imports: [
     BrowserModule
