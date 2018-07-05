3883597265b1bca2313a076bcb05ea839222cb78
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-04 23:36:42 -0700

    route: added a new route. If the url follows that pattern, that route will be triggered and the param will be parsed. Now if we click on a course card button, we will be navigated to the path to that course and it displays the name of that course.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { WhiteBoardComponent } from './white-board/white-board.component';
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: WhiteBoardComponent},
+  { path: 'course/:courseId', component: CourseViewerComponent},
   { path: '**', component: WhiteBoardComponent} // any other path goes here.
+  // if the path following the pattern, a course/ followed by a courseId, go CourseViewerComponent
 
 ];
 
--- src/app/course-viewer/course-viewer.component.ts
+++ src/app/course-viewer/course-viewer.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
+import { CourseServiceClient } from '../services/course.service.client';
+import { Course } from '../models/course.model.client';
 @Component({
   selector: 'app-course-viewer',
   templateUrl: './course-viewer.component.html',
   styleUrls: ['./course-viewer.component.css']
 })
 export class CourseViewerComponent implements OnInit {
+  // ActivatedRoute is a listner to the url. if url changes we will get notified.
+  constructor(private service: CourseServiceClient,
+              private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.loadCourse(params.courseId)); // if any change, we will get notified.
+  }
 
-  constructor() { }
-
+  // load course for courseId
+  course: Course = new Course();
+  loadCourse(courseId){
+    this.service.findCourseById(courseId)
+      .then(course => this.course = course);
+  }
   ngOnInit() {
   }
 
