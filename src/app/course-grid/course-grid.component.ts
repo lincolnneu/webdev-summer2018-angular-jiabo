c419ead1fc90d38d53ac506f5fee6fa09ec9037f
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-04 22:35:30 -0700

    whiteboard: course-grid: injected CourseServiceClient to the course-grid component. Used strong type course class. Improvced styling in the css file.

--- src/app/course-grid/course-grid.component.ts
+++ src/app/course-grid/course-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CourseServiceClient } from '../services/course.service.client';
+import { Course } from '../models/course.model.client';
 @Component({
   selector: 'app-course-grid',
   templateUrl: './course-grid.component.html',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseGridComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: CourseServiceClient) { }
+
+  courses: Course[] = [];
+  //strongly type of data type. Cannot assign wrong type of data.
+  // Attempt to add a different field that is not the original data type will fail.
+  // Catch this error early before sent to server.
 
   ngOnInit() {
+    this.service.findAllCourses()
+      .then(courses => this.courses = courses);
   }
 
 }
