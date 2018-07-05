24a39693bc27a57d818d5489a9bbd5736d73e044
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-04 22:05:43 -0700

    course service: added a service for exclusively for course service. ng generate component course-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloWordComponent } from './hello-word/hello-word.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { CourseNavigatorServiceClient } from './services/course-navigator.service.client';
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 import { WhiteBoardComponent } from './white-board/white-board.component';
+import { CourseServiceClient } from './services/course.service.client';
+import { CourseGridComponent } from './course-grid/course-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWordComponent,
     CourseNavigatorComponent,
-    WhiteBoardComponent
+    WhiteBoardComponent,
+    CourseGridComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [
-    CourseNavigatorServiceClient
+    CourseNavigatorServiceClient,
+    CourseServiceClient
   ],
   bootstrap: [AppComponent]
 })
