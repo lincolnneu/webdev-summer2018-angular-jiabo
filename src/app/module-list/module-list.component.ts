70dc1dcbcda4f1d5c6e704bd9f1232715d1209f8
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-16 10:19:30 -0700

    component is aware of changes in the URL and it takes action about it

--- src/app/module-list/module-list.component.ts
+++ src/app/module-list/module-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-module-list',
   templateUrl: './module-list.component.html',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModuleListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.loadModules(params['courseId']));
+   } // component is aware of changes in the URL and it takes action about it.
+   // we load modules for that particular course
+
+   courseId;
+   loadModules(courseId) {
+      this.courseId = courseId;
+   }
 
   ngOnInit() {
   }
