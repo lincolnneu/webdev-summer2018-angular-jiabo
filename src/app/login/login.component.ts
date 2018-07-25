208f33a98a90c4dbda3d6ac153503fd6714d31d0
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-25 11:33:42 -0700

    added login template

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username;
+  password;
+  login(username, password){
+  console.log([username, password]);}
+
   constructor() { }
 
   ngOnInit() {
