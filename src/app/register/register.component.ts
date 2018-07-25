be5a913522e9c54d5b0c4ea59b4fa143b3fffe90
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-25 11:38:43 -0700

    added register template

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
   constructor() { }
-
+  username;
+  password;
+  password2;
+  register(username, password, password2){
+    console.log([username, password, password2]);
+  }
   ngOnInit() {
   }
 
