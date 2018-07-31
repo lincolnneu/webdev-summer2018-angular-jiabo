cb0747f94edbfab363e7e606bf1838d50fb2dd18
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-30 18:33:24 -0700

    fix a bug in fetching lesson

--- src/app/services/lesson.service.client.ts
+++ src/app/services/lesson.service.client.ts
@@ -1,5 +1,5 @@
 import * as constants from '../constants';
-const LESSON_API = constants.HOST + '/course/';
+const LESSON_API = constants.HOST + '/api/course/';
 export class LessonServiceClient {
   findLessonsForModule(courseId, moduleId) {
     return fetch( LESSON_API + courseId + '/module/' + moduleId + '/lesson')
